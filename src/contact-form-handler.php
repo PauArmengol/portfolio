a3fa977903c8f5b1deda5872c77a4be49a23342b
paula armengol <p.armengol6@gmail.com>
2023-05-13 20:23:37 -0300

    mejoras php

--- src/contact-form-handler.php
+++ src/contact-form-handler.php
@@ -2,7 +2,7 @@
     $to = "p.armengol6@gmail.com"; // Cambia "tu_correo_electronico_aqui" con tu dirección de correo electrónico
     $subject = "Nuevo mensaje del formulario de contacto"; // Cambia el asunto del correo electrónico si lo deseas
     $name = $_POST['name'];
-    $email = $_POST['email_address'];
+    $email = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
     $headers = "From: " . $name . " <" . $email . ">\r\n";
